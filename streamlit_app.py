a96d80e901a6841fe10ce5b9d42e472988b00f23
RathnasriB <127265902+RathnasriB@users.noreply.github.com>
2023-04-13 13:59:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 2 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
